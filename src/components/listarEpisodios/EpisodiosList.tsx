1febe195deb3d75cff239c22769d314c0aff1ab8
Jssoff <js.garay@uniandes.com>
2025-09-24 21:45:43 -0500

    listarEps

---
+++ src/components/listarEpisodios/EpisodiosList.tsx
@@ -0,0 +1,19 @@
+import { Episode } from '@/types/interfaces';
+import EpisodeCard from '../ui/Card';
+
+interface ListaEpsProps {
+  episodes: Episode[];
+}
+
+export default function EpisodiosList({ episodes }: ListaEpsProps) {
+
+  return (
+    <div className="w-11/12 max-w-lg mx-auto">
+      <div className="space-y-6">
+        {episodes.map((episode) => (
+          <EpisodeCard key={episode.id} episodio={episode} />
+        ))}
+      </div>
+    </div>
+  );
+}
